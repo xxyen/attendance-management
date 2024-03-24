d231a3acd347373848f1870be6a183ff8195f2e3
Jiazheng Sun <js1106@duke.edu>
2024-03-24 15:38:30 -0400

    Minor changes to Email Notification

--- attendance-management/shared/src/main/java/edu/duke/ece651/shared/EmailNotification.java
+++ attendance-management/shared/src/main/java/edu/duke/ece651/shared/EmailNotification.java
@@ -87,29 +87,6 @@ public class EmailNotification implements Notification {
     return credential;
   }
 
-  /*
-  public static void main(String... args) throws IOException, GeneralSecurityException {
-    // Build a new authorized API client service.
-    final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-    Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-        .setApplicationName(APPLICATION_NAME)
-        .build();
-
-    // Print the labels in the user's account.
-    String user = "me";
-    ListLabelsResponse listResponse = service.users().labels().list(user).execute();
-    List<Label> labels = listResponse.getLabels();
-    if (labels.isEmpty()) {
-      System.out.println("No labels found.");
-    } else {
-      System.out.println("Labels:");
-      for (Label label : labels) {
-        System.out.printf("- %s\n", label.getName());
-      }
-    }
-  }
-  */
-
   public void sendEmail(String subject, String bodyText) throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
