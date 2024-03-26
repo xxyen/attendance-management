c966e0183a8cbf29a9bdfdd3d8b2d20f36b137b1
xinyi.xie2@duke.edu <xx98@duke.edu>
2024-03-26 16:40:23 -0400

    update test.sh

--- attendance-management/scripts/test.sh
+++ attendance-management/scripts/test.sh
@@ -3,4 +3,4 @@
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
 scripts/coverage_summary.sh
-cp -r app/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
