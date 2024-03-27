91a73927b72a2fefa25e7bd106adff852cf6e60b
wille <cp357@duke.edu>
2024-03-26 22:37:12 -0400

    add weeklyReporter

--- attendance-management/shared/src/test/java/edu/duke/ece651/shared/EmailTest.java
+++ attendance-management/shared/src/test/java/edu/duke/ece651/shared/EmailTest.java
@@ -11,9 +11,27 @@ class EmailTest {
         Email e1 = new Email("abc@duke.edu");
         assertEquals(e1.getEmailAddr(), "abc@duke.edu");
 
-        assertThrows(IllegalArgumentException.class, ()->new Email(""));
+        String test = null;
+        assertThrows(IllegalArgumentException.class, ()->new Email(test));
         assertThrows(IllegalArgumentException.class, ()->new Email("ddc gmail.com"));
         assertThrows(IllegalArgumentException.class, ()->new Email("ewc@gmail..com"));
+
+
+    }
+
+    @Test
+    void test_equalsAndHash(){
+        Email e1 = new Email("abc@duke.edu");
+        Email e2 = new Email("abc@duke.edu");
+        Email e3 = new Email("def@duke.edu");
+        Email e4 = null;
+        String e5 = "abc@duke.edu";
+        assertTrue(e1.equals(e1));
+        assertTrue(e1.equals(e2));
+        assertFalse(e1.equals(e3));
+        assertFalse(e1.equals(e4));
+        assertFalse(e1.equals(e5));
+
 
 
     }
