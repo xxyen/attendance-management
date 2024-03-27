79f59d4004b0936a26ba5ce4243f8b74b3fcba5f
wille <cp357@duke.edu>
2024-03-27 03:26:14 -0400

    add doc

--- attendance-management/shared/src/main/java/edu/duke/ece651/shared/Email.java
+++ attendance-management/shared/src/main/java/edu/duke/ece651/shared/Email.java
@@ -3,14 +3,29 @@ package edu.duke.ece651.shared;
 import java.util.Objects;
 import java.util.regex.*;
 
+/**
+ * This class represent email address.
+ * @author Can Pei
+ * @version 1.0
+ */
 public class Email {
+    /**
+     * Email address
+     */
     private String emailAddr;
 
+    /**
+     * Regular expression of legal email address syntax
+     */
     private static final String EMAIL_REGEX =
             "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
+    /**
+     * Check if a string is of valid email address syntax.
+     * @param email is the string to check.
+     */
     public static boolean checkValid(String email) {
         if (email == null) {
             return false;
@@ -18,7 +33,9 @@ public class Email {
         Matcher matcher = EMAIL_PATTERN.matcher(email);
         return matcher.matches();
     }
-
+    /**
+     * Constructor
+     */
     public Email(String emailAddr) {
         if (checkValid(emailAddr)) {
             this.emailAddr = emailAddr;
@@ -30,10 +47,16 @@ public class Email {
         }
     }
 
+    /**
+     * Email getter
+     */
     public String getEmailAddr() {
         return emailAddr;
     }
 
+    /**
+     * Email equal operator
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -42,6 +65,9 @@ public class Email {
         return Objects.equals(emailAddr, email.emailAddr);
     }
 
+    /**
+     * Email hashcode
+     */
     @Override
     public int hashCode() {
         return Objects.hash(emailAddr);
