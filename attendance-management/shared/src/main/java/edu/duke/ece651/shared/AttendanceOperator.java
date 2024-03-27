923e734c00ccb1a400de84a33af00d7a3683f3a0
jiazheng.sun@duke.edu <js1106@duke.edu>
2024-03-27 18:26:04 -0400

    Update AttendanceOperator.java

--- attendance-management/shared/src/main/java/edu/duke/ece651/shared/AttendanceOperator.java
+++ attendance-management/shared/src/main/java/edu/duke/ece651/shared/AttendanceOperator.java
@@ -1,7 +1,20 @@
 package edu.duke.ece651.shared;
 
 public interface AttendanceOperator {
+  /**
+   * Create a single attendance record given student and status.
+   * @param student is the student to record
+   * @param status is the status to record
+   * @return a single attendace record
+   */
   public AttendanceRecord takeAttendance(Student student, Status status);
   // Return true if successfully changed record, otherwise return false
+  /**
+   * Change a single record given which session, student, and what's the new status
+   * @param session is the chosen session
+   * @param student is which student to change status
+   * @param newStatus is the target status
+   * @return true if successfully changed status, otherwise return false
+   */
   public boolean changeSingleRecord(Session session, Student student, Status newStatus);
 }
