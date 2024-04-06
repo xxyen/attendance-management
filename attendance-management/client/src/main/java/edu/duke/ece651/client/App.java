14d02ee3c0831f70e7d87ff65675f2d408737225
wille <cp357@duke.edu>
2024-04-05 22:04:52 -0400

    add login loop in server

--- attendance-management/client/src/main/java/edu/duke/ece651/client/App.java
+++ attendance-management/client/src/main/java/edu/duke/ece651/client/App.java
@@ -3,12 +3,27 @@
  */
 package edu.duke.ece651.client;
 
+import java.io.IOException;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String[] args) throws IOException {
+        try {
+            ClientSocketHandler client = new ClientSocketHandler();
+            String host = "localhost";
+            int port = 1234;
+            String testxml = "<request><message>Hello from Can Pei</message></request>";
+
+            client.connectToServer(host, port);
+            client.sendXmlToServer(testxml);
+            client.readResponseFromServer();
+            client.closeConnection();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
