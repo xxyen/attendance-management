8aad6a75549445d4e515c6202e784ce46f518e0c
Jiazheng Sun <js1106@duke.edu>
2024-04-06 21:32:18 -0400

    Trying to fix pipeline

---
+++ attendance-management/courseManage/src/test/java/edu/duke/ece651/courseManage/courseManagerTest.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.courseManage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class courseManagerTest {
+  @Test
+  public void test_() {
+
+  }
+
+}
