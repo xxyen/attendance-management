3b7841ebe423538273d53ed90f5aa809de400208
Jiazheng Sun <js1106@duke.edu>
2024-04-29 16:36:19 -0400

    Removed courseManager

--- attendance-management/courseManage/src/test/java/edu/duke/ece651/courseManage/AppTest.java
+++ attendance-management/courseManage/src/test/java/edu/duke/ece651/courseManage/AppTest.java
@@ -41,30 +41,6 @@ import org.testfx.api.FxRobot;
 @ExtendWith(ApplicationExtension.class)
 public class AppTest {
   App a;
-  @Disabled
-  @Test
-  @ResourceLock(value = Resources.SYSTEM_OUT, mode = ResourceAccessMode.READ_WRITE)
-  public void test_App() throws IOException {
-    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-    PrintStream out = new PrintStream(bytes, true);
-    InputStream input = getClass().getClassLoader().getResourceAsStream("input5.txt");
-    assertNotNull(input);
-    InputStream expectedStream = getClass().getClassLoader().getResourceAsStream("output5.txt");
-    assertNotNull(expectedStream);
-    InputStream oldIn = System.in;
-    PrintStream oldOut = System.out;
-    try {
-      System.setIn(input);
-      System.setOut(out);
-      //App.main(new String[0]);
-    }
-    finally {
-      System.setIn(oldIn);
-      System.setOut(oldOut);
-    }
-    String expected = new String(expectedStream.readAllBytes());
-    String actual = bytes.toString();
-  }
 
   @Start
   public void start(Stage stage) throws IOException {
